5d0447c27c91883ebb79b83658f185cf9f82e3e4
LebogangMputle <156669179+LebogangMputle@users.noreply.github.com>
2024-02-19 09:55:45 +0200

    Update index.js

--- my portfolio/index.js
+++ my portfolio/index.js
@@ -1,6 +1,14 @@
-function toggleMenu () {
-    const navToggle = document.querySelector('.hamburger');
-    const navLinks = document.querySelectorAll('.nav__list');
-    navToggle.classList.toggle("open")
-    navLinks.classList.toggle("open")
-}
+// Select the navigation toggle button
+const navToggle = document.querySelector('.nav-toggle');
+// Select all navigation links
+const navLinks = document.querySelectorAll('.nav__link')
+// Event listener for toggling the navigation menu
+navToggle.addEventListener('click', () => {         // Toggle the 'nav-open' class on the body element to show/hide the navigation menu
+    document.body.classList.toggle('nav-open');
+});                                                 // Event listeners for each navigation link to close the navigation menu when a link is clicked
+
+navLinks.forEach(link => {
+    link.addEventListener('click', () => {          // Remove the 'nav-open' class from the body element to hide the navigation menu
+        document.body.classList.remove('nav-open');
+    })
+})
